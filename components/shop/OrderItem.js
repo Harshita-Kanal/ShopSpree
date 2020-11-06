79b56521decbd81a76c7e0b0ffc7d12472a70a2e
Harshita-Kanal <harshita.kgv@gmail.com>
2020-11-06 22:19:59 +0530

    Added order screen

---
+++ components/shop/OrderItem.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import Colors from "../../constants/Colors";
+import CartItem from "./CartItem";
+
+const OrderItem = (props) => {
+  const [showDetails, setShowDetails] = useState(false);
+  return (
+    <View style={styles.orderItem}>
+      <View style={styles.summary}>
+        <Text style={styles.totalAmount}>Rs.{props.amount.toFixed(2)} </Text>
+        <Text style={styles.date}>{props.date}</Text>
+      </View>
+      <View style={styles.buttonItem}>
+        <Button
+          color={Colors.primary}
+          title="Show Details"
+          onPress={() => {
+            setShowDetails((prevState = !prevState));
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  orderItem: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
+    padding: 10,
+    margin: 20,
+    fontFamily: "sansita-bold",
+  },
+  summary: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  },
+  totalAmount: {
+    fontFamily: "sansita-bold",
+    fontSize: 16,
+  },
+  date: {
+    fontSize: 16,
+    fontFamily: "sansita-bold",
+  },
+  buttonItem: {
+    width: "40%",
+  },
+});
+
+export default OrderItem;
--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -3,13 +3,26 @@ import { View, FlatList, Text } from "react-native";
 import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
+
 const OrdersScreen = (props) => {
   const orders = useSelector((state) => state.orders.orders);
   return (
     <FlatList
       data={orders}
-      //   keyExtractor={itemData.item.id}
-      renderItem={(itemData) => <Text>{itemData.item.id}</Text>}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.date.toLocaleDateString("en-EN", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        />
+      )}
     />
   );
 };
